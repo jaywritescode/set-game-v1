c3168e593b27de0bcfc6e8164cb6bce7d12819a0
Jay Harris <jayharris@gmail.com>
2015-11-04 16:25:34 -0500

    remove a bunch of arbitrary debugging-help print statements

--- solitaire.py
+++ solitaire.py
@@ -16,11 +16,6 @@ class SolitaireSet:
 
         self.sets = find_all_sets(self.cards)
 
-        for _set in self.sets:
-            print(_set)
-
-        print("\n" + ("=" * 80) + "\n")
-
         if len(self.sets) == self.num_sets:
             return
 
@@ -29,18 +24,7 @@ class SolitaireSet:
             for _card in _set.cards:
                 cards_to_sets_map[_card].add(_set)
 
-        for _card, _set_of_sets in cards_to_sets_map.items():
-            st = str(_card) + " -> "
-            for _set in _set_of_sets:
-                st += str(_set) + " "
-            print(st)
-
         cards_not_in_sets = list(self.cards - cards_to_sets_map.keys())
-
-        for _card in cards_not_in_sets:
-            print(_card)
-
-        print("\n" + ("=" * 80) + "\n")
 
         cards_to_complete_sets = defaultdict(set)
         for pair in itertools.combinations(list(self.cards), 2):
@@ -50,26 +34,17 @@ class SolitaireSet:
             potential_set = self.set_factory.make_set_from_cards(list(pair) + [completion_card])
             cards_to_complete_sets[completion_card].add(potential_set)
 
-        for _card, _set_of_sets in cards_to_complete_sets.items():
-            st = str(_card) + " -> "
-            for _set in _set_of_sets:
-                st += str(_set) + " "
-            print(st)
-
-        print("\n" + ("=" * 80) + "\n")
-
         if len(self.sets) > self.num_sets:
             pass
         else:
             sets_needed = self.num_sets - len(self.sets)
 
-
-            potential_replacements = [card for card in cards_to_complete_sets if len(cards_to_complete_sets[card]) == sets_needed]
+            potential_replacements = [card for card in cards_to_complete_sets
+                                      if len(cards_to_complete_sets[card]) == sets_needed]
             if not len(potential_replacements):
                 raise Exception
             replacement_card = random.choice(potential_replacements)
 
-            # you can't include the card you're removing in the new sets that you're counting!
             for _set in cards_to_complete_sets[replacement_card]:
                 for _card in _set.cards:
                     if _card in cards_not_in_sets:
