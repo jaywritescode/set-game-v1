be4f55ee4cf1affc6fc3c7ecf5e7913af34189be
Jay Harris <jayharris@gmail.com>
2015-11-19 16:56:16 -0500

    after result, deselect all cards and clear selected set

--- public/js/src/solitaire.js
+++ public/js/src/solitaire.js
@@ -18,7 +18,8 @@ var Solitaire = React.createClass({
   getInitialState() {
     return {
       cards: [],
-      selected: new Set()
+      selected: new Set(),
+      found: new Set()
     };
   },
 
@@ -40,7 +41,24 @@ var Solitaire = React.createClass({
         method: 'PUT',
         contextType: 'application/json'
       }).then((response) => {
-        console.log(response);
+        switch(response['result']) {
+          case 'OK':
+            console.log('OK'); break;
+            this.state.found.add(new Set(this.state.selected));
+          case 'NOT_A_SET':
+            console.log('NOT_A_SET'); break;
+          case 'ALREADY_FOUND':
+            console.log('ALREADY_FOUND'); break;
+          default:
+            throw("This shouldn't happen.");
+        }
+        console.log(this.state.selected);
+        for (card of this.state.selected) {
+          card.setState({
+            selected: false
+          });
+        }
+        this.state.selected.clear();
       }, (response) => {
         console.error(response);
       });
