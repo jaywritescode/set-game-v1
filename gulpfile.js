8f203eb56b612f527ea7bd34b6ed078b2701ca96
Jay Harris <jayharris@gmail.com>
2015-11-26 19:53:10 -0500

    Add sourcemaps and watch to gulpfile.
    Fixes #8, #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', () => {
-  return gulp.src('public/js/src/solitaire.js')
+gulp.task('default', ['watch']);
+
+gulp.task('babel', () => {
+  return gulp.src('public/js/src/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['react']
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public/js/build'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('public/js/src/*.js', ['babel']);
 });
