afbc9422cc372ef40f2b5476097d5f12848eb6ab
Jay Harris <jayharris@gmail.com>
2015-12-31 23:11:45 -0500

    move SetCard to its own file to share

--- public/js/src/solitaire.js
+++ public/js/src/solitaire.js
@@ -6,6 +6,8 @@ import { render } from 'react-dom';
 import { Modal, Button } from 'react-bootstrap';
 import moment from 'moment';
 
+import SetCard from 'setcard';
+
 const IMG_PATH = 'static/img/';
 
 class Solitaire extends React.Component {
@@ -151,42 +153,6 @@ class Solitaire extends React.Component {
         </div>
         {this.renderSetsFound()}
       </div>
-    );
-  }
-}
-
-class SetCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: false
-    };
-  }
-
-  handleClick(e) {
-    let newState = {
-      selected: !this.state.selected
-    };
-    this.setState(newState);
-    this.props.parentHandleClick(this, newState);
-  }
-
-  content() {
-    let card = this.props.card;
-    let filename = `${IMG_PATH}cards/${card.number}-${card.color}-${card.shading}-${card.shape}s.png`;
-
-    return (
-      <img src={filename} />
-    );
-  }
-
-  render() {
-    let classes = ["card"];
-    if (this.state.selected) {
-      classes.push("selected");
-    }
-    return (
-      <div className={classes.join(" ")} onClick={this.handleClick.bind(this)}>{this.content()}</div>
     );
   }
 }
