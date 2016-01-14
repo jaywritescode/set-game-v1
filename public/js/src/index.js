b3bb5757eec1f54c8dc53c2d67427a3876e22ee0
Jay Harris <jayharris@gmail.com>
2016-01-14 12:54:38 -0500

    list multiplayer games attached to the web service

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -3,12 +3,33 @@
 import $ from 'jquery';
 import React from 'react';
 import { render } from 'react-dom';
-import { Button } from 'react-bootstrap';
+import { Button, SplitButton, MenuItem } from 'react-bootstrap';
 
 import Solitaire from 'solitaire';
 import Multiplayer from 'multiplayer';
 
 class SetApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: {}
+    };
+  }
+
+  componentWillMount() {
+    let onSuccess = function(response) {
+      this.setState({
+        'games': response
+      });
+    }.bind(this);
+    let onError = function(response) {
+      this.setState({
+        'error': response
+      });
+    }.bind(this);
+    $.get('/multiplayer').then(onSuccess, onError);
+  }
+
   startSolitaire() {
     $.get('/solitaire').then((response) => {
       render(
@@ -28,8 +49,23 @@ class SetApp extends React.Component {
   render() {
     return (
       <div id="index-wrapper">
-        <Button bsSize="large" bsStyle="default" onClick={this.startSolitaire}>Solitaire Set</Button>
-        <Button bsSize="large" bsStyle="default" onClick={this.startMultiplayer}>Multiplayer Set</Button>
+        <Button bsSize="large"
+                bsStyle="default"
+                key="solitaire"
+                onClick={this.startSolitaire}
+                id="solitaire_button">Solitaire Set</Button>
+        <SplitButton bsSize="large"
+                     bsStyle="default"
+                     key="multiplayer"
+                     title="Multiplayer Set"
+                     id="multiplayer_split_button"
+                     onClick={this.startMultiplayer}>
+          {$.map(this.state.games, (value, key) => {
+            return (
+              <MenuItem eventKey={key} key={key}>{`${key} => ${value} player${value == 1 ? '' : 's'}`}</MenuItem>
+            );
+          })}
+        </SplitButton>
       </div>
     );
   }
