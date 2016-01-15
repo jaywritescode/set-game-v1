61df41c59aade92ad9472af77688d295c99dfa64
Jay Harris <jayharris@gmail.com>
2016-01-15 12:53:21 -0500

    separate creating a game from checking the session to see if any exists

--- webservices/multiplayergame.py
+++ webservices/multiplayergame.py
@@ -18,12 +18,40 @@ class MultiplayerWebService:
 
     @cherrypy.tools.json_out()
     def PUT(self):
-        for _ in range(5):
-            self.create()
+        pass
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def create(self):
-        name = haikunate()
-        self.games[name] = MultiplayerSet()
-        return {'name': name}
+        """
+        Endpoint to create a new MultiplayerSet game.
+
+        :return: the name of the newly created game
+        """
+        if cherrypy.session.get('game'):
+            raise cherrypy.HTTPError(400, 'Already in a game')
+        if cherrypy.session.get('player'):
+            raise cherrypy.HTTPError(400, 'Already has a player')
+
+        name = self.make_name()
+        game = self.games[name] = cherrypy.session['game'] = self.create_game()
+        cherrypy.session['player'] = game.add_player()
+        return {
+            'name': name,
+            'player': cherrypy.session['player'].id
+        }
+
+
+    def create_game(self):
+        """
+        Creates a game.
+
+        :return: the game's name and the newly created game
+        """
+        return MultiplayerSet()
+
+    def make_name(self):
+        while True:
+            name = haikunate()
+            if name not in self.games:
+                return name
