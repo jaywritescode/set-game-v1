0a05cf86eb171475d47fc404114d7c6eabec6837
Jay Harris <jayharris@gmail.com>
2016-02-21 11:20:03 -0500

    delete mixins.py

--- webservices/mixins.py
+++
@@ -1,5 +0,0 @@
-from app.setutils import Card
-
-def json_to_cards(blob):
-    return [Card(*[getattr(Card, key)(obj[key])
-                   for key in ['number', 'color', 'shading', 'shape']]) for obj in blob]
