3981a219b7b3b9b3246e8724a5710ec653f41983
Jay Harris <jaywritescode@users.noreply.github.com>
2017-06-18 23:09:49 -0400

    test selecting three cards that make a set

--- tests/solitaire.js
+++ tests/solitaire.js
@@ -27,6 +27,30 @@ module.exports = {
     ].forEach((str) => {
       client.expect.element(`.card img[src*=${str}]`).to.be.present;
     });
+  },
+
+  'Choose three cards that make a set': function(client) {
+    client.useXpath();
+
+    client.click('//*[@id="cards"]/ul/li/div/img[contains(@src, "one-blue-striped-squiggles")]');
+    client.expect.element('//*[@id="cards"]/ul/li/div/img[contains(@src, "one-blue-striped-squiggles")]/..')
+      .to.have.attribute('class').which.contains('selected');
+
+    client.click('//*[@id="cards"]/ul/li/div/img[contains(@src, "two-blue-striped-ovals")]');
+    client.expect.element('//*[@id="cards"]/ul/li/div/img[contains(@src, "two-blue-striped-ovals")]/..')
+      .to.have.attribute('class').which.contains('selected');
+
+    client
+      .click('//*[@id="cards"]/ul/li/div/img[contains(@src, "three-blue-striped-diamonds")]')
+      .waitForElementVisible('//*[@id="found-so-far"]/ul[1]', 1000);
+
+    client.expect.element('//*[@id="found-so-far"]/ul[1]/li/div/img[contains(@src, "one-blue-striped-squiggles")]')
+      .to.be.visible;
+    client.expect.element('//*[@id="found-so-far"]/ul[1]/li/div/img[contains(@src, "two-blue-striped-ovals")]')
+      .to.be.visible;
+    client.expect.element('//*[@id="found-so-far"]/ul[1]/li/div/img[contains(@src, "three-blue-striped-diamonds")]')
+      .to.be.visible;
+
     client.end();
   }
 }
